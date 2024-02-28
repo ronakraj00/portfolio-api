93273c46cfaefdd625dba4501514229c74dd0c59
ronak raj <126279165+ronakraj00@users.noreply.github.com>
2024-02-29 01:59:57 +0530

    connect mongodb

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+const mongoose = require("mongoose");
 const express = require("express");
+
 const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.json("hi");
+    res.send("hi");
 });
+
+const mongoDB =
+    "mongodb+srv://admin:ronakraj1@cluster0.bjksk03.mongodb.net/authentication?retryWrites=true&w=majority";
+
+(async function main() {
+    await mongoose.connect(mongoDB);
+    console.log("connected");
+})().catch((err) => console.log(err));
 
 app.listen(PORT, () => console.log("Server listening at localhost:" + PORT));
