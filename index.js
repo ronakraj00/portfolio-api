afc5016178d58f7eb5cee0a6387066cc6a4b228b
ronak raj <126279165+ronakraj00@users.noreply.github.com>
2024-02-29 18:02:09 +0530

    create env for PORT

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const sites = require("./data.json");
-
+require("dotenv").config();
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 
 app.get("/", (req, res) => {
     res.json(sites);
@@ -14,9 +14,11 @@ app.get("/", (req, res) => {
 const mongoDB =
     "mongodb+srv://admin:ronakraj1@cluster0.bjksk03.mongodb.net/authentication?retryWrites=true&w=majority";
 
-(async function main() {
-    await mongoose.connect(mongoDB);
-    console.log("connected");
-})().catch((err) => console.log(err));
+// (async function main() {
+//     await mongoose.connect(mongoDB);
+//     console.log("connected");
+// })().catch((err) => console.log(err));
 
-app.listen(PORT, () => console.log("Server listening at localhost:" + PORT));
+app.listen(process.env.PORT, () =>
+    console.log("Server listening at localhost:" + PORT)
+);
